7a4e8d3e6365012f060ade044aab3da8ee268515
Evan Louie <Evan Louie>
2016-02-27 18:34:12 -0800

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
     skel.breakpoints({
         xlarge: '(max-width: 1680px)',
         large: '(max-width: 1080px)',
@@ -6,14 +6,14 @@
         small: '(max-width: 736px)',
         xsmall: '(max-width: 480px)'
     });
-    $(function() {
+    $(function () {
         var $window = $(window),
             $body = $('body'),
             page = $body.attr('class').split(' ')[0];
         if (skel.vars.mobile) $body.addClass('is-touch');
         $body.addClass('is-loading');
-        $window.on('load', function() {
-            window.setTimeout(function() {
+        $window.on('load', function () {
+            window.setTimeout(function () {
                 $body.removeClass('is-loading');
             }, 0);
         });
@@ -22,7 +22,7 @@
                 if (!$body.hasClass('is-touch')) {
                     var f = 10,
                         $bg = $('#bg');
-                    $window.on('mousemove.n33 mouseenter.n33 mouseleave.n33', function(event) {
+                    $window.on('mousemove.n33 mouseenter.n33 mouseleave.n33', function (event) {
                         var x = event.pageX,
                             y = event.pageY,
                             ww = $window.width(),
